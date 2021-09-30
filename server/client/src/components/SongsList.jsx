8151513086f1a3d9c816d0b976a516b0990d3582
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-09-30 15:19:11 +0200

    destructure songs list page

---
+++ server/client/src/components/SongsList.jsx
@@ -0,0 +1,40 @@
+import logo from "../logo.svg";
+import "../App.css";
+
+
+import React from 'react';
+import { useState, useEffect } from "react";
+// import the service file since we need it to send (and get) the data to(from) the server
+import service from '../api/service';
+
+
+function SongsList() {
+  
+ 
+    const [allMidi, setAllMidi] = useState([]);
+    
+    const getAllMidi = () => {
+      return service
+        .findAllMidi()
+        .then(response => {
+          console.log("response is: ", response);
+          setAllMidi(response)
+        })
+        .catch(err => console.log('Error while uploading the file: ', err));  
+    }
+
+    useEffect(() => {
+      getAllMidi()
+  }, [])
+
+  const songsList = allMidi.map(song => <><h1>{song.title}</h1><a href={song.songUrl}>Download</a></>)
+  
+  
+  return (<>
+  <h1>List of Songs</h1>
+  {songsList}
+  </>
+  );
+}
+
+export default SongsList;
