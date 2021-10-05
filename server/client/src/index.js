723cf073fd68e11e7c98a00b56fad1ff1f1f4042
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2021-10-05 10:14:32 +0200

    added /loggedin route to index.js

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import axios from 'axios'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+axios.get('http://localhost:5005/api/auth/loggedin')
+  .then(response => {
+    console.log('logged in user: ', response.data);
+    const user = response.data;
+    ReactDOM.render(
+      <Router>
+        <App user={user} />
+      </Router>,
+      document.getElementById('root')
+    );
+  })
